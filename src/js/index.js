d8e38a9955c50e8776a187804336b8e9d410ea4c
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2020-12-06 19:22:20 +0100

    final animation changes&js div adding

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,26 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+function renderHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+
+    document.body.appendChild(heart);
+    return heart;
+}
+
+function renderHeartContainer(heart) {
+    const heartContainer = document.createElement('div');
+    heartContainer.classList.add('heart__container');
+
+    const img = document.createElement('img');
+    img.src = 'src/assets/img/heart.svg';
+    img.classList.add('heart__container-image');
+
+    heartContainer.appendChild(img);
+    heart.appendChild(heartContainer);
+}
+
+const heart = renderHeart();
+renderHeartContainer(heart);
