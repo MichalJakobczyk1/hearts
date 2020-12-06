4d0e874393026fbccc8772acdf8fa2e6caa20848
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2020-12-06 19:45:43 +0100

    final touches & PWA

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
 
 /* place your code below */
 
@@ -20,12 +20,18 @@ function renderHeartContainer(heart) {
     const heartContainer = document.createElement('div');
     heartContainer.classList.add('heart__container');
 
+    heartContainer.style.left = `${Math.random() * 100}%`;
+
     const img = document.createElement('img');
     img.src = 'src/assets/img/heart.svg';
     img.classList.add('heart__container-image');
 
+    img.style.transform = `scale(${Math.random()})`;
+
     heartContainer.appendChild(img);
     heart.appendChild(heartContainer);
+
+    setTimeout(renderHeartContainer, 500, heart);
 }
 
 const heart = renderHeart();
