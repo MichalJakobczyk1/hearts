6d66226c1c6edc18a55b29b81d1412993f66eb44
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2020-12-15 20:42:29 +0100

    audio toggle

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,59 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
 
 function renderHeart() {
-    const heart = document.createElement('div');
-    heart.classList.add('heart');
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
 
-    document.body.appendChild(heart);
-    return heart;
+  document.body.appendChild(heart);
+  return heart;
 }
 
 function renderHeartContainer(heart) {
-    const heartContainer = document.createElement('div');
-    heartContainer.classList.add('heart__container');
+  const heartContainer = document.createElement("div");
+  heartContainer.classList.add("heart__container");
 
-    heartContainer.style.left = `${Math.random() * 100}%`;
+  heartContainer.style.left = `${Math.random() * 100}%`;
 
-    const img = document.createElement('img');
-    img.src = 'heart.svg';
-    img.classList.add('heart__container-image');
+  const img = document.createElement("img");
+  img.src = "heart.svg";
+  img.classList.add("heart__container-image");
 
-    img.style.transform = `scale(${Math.random()})`;
+  img.style.transform = `scale(${Math.random()})`;
 
-    heartContainer.appendChild(img);
-    heart.appendChild(heartContainer);
+  heartContainer.appendChild(img);
+  heart.appendChild(heartContainer);
 
-    setTimeout(renderHeartContainer, 500, heart);
+  setTimeout(renderHeartContainer, 500, heart);
 }
 
 const heart = renderHeart();
-// renderHeartContainer(heart);
+renderHeartContainer(heart);
 
-const button = document.querySelector(".play--js");
+const button = document.querySelector(".button--play");
+const audio = document.createElement("audio");
 
-function renderAudio() {
-    const audio = document.createElement('audio');
-    audio.src = 'circles.mp3';
+button.addEventListener("click", () => {
+  if (button.classList.contains("button--play")) {
+    audio.classList.add("audio");
+    audio.src = "../public/circles.mp3";
     audio.play();
+    audio.volume = 0.1;
 
     document.body.appendChild(audio);
-};
 
-button.addEventListener("click",(renderAudio));
+    button.classList.add("button--pause");
+    button.classList.remove("button--play");
+  } else {
+    audio.pause();
+    button.classList.add("button--play");
+    button.classList.remove("button--pause");
+  }
+});
